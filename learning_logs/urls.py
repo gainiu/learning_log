4fe3b9505530dfdfe763982b637e59dea3d8465a
gainiu <276647369@qq.com>
2020-11-05 01:22:31 +0800

    update

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -16,4 +16,7 @@ urlpatterns=[
 
     #用于添加新主题的网页
     path('new_topic/',views.new_topic,name='new_topic'),
+
+    #用于添加新条目的页面
+    path('new_entry/<topic_id>/',views.new_entry,name='new_entry'),
 ]
