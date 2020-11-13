7c763fb4b4a3e21b39c5f47bfbd7d6bf432df215
gainiu <276647369@qq.com>
2020-11-14 01:13:48 +0800

    add bootstrap

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #第三方应用程序
+    'bootstrap3',
     #My apps
     'learning_logs',
-    'users'
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,8 @@ STATIC_URL = '/static/'
 
 #我的设置
 LOGIN_URL='/users/login/'
+
+#django-bootstrap3的设置
+BOOTSTRAP3={
+    'include_jquery':True,
+}
